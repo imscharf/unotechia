6893fe045a41e4fff880b8d95873b1a05d34333d
João Pedro Scharf <100308657+imscharf@users.noreply.github.com>
2025-10-29 12:58:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Plus, Wrench, Brain, Search } from 'lucide-react';
 import { Modal } from './components/Modal';
 import { ParafusoForm } from './components/ParafusoForm';
